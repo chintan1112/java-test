609d6dea95c5308e599d8840755865c87d722a83
Chintan Mistry <999.chintan@gmail.com>
2022-04-17 15:43:13 +0530

    added get endpoint

--- src/main/java/com/galaxy/java/JavaApplication.java
+++ src/main/java/com/galaxy/java/JavaApplication.java
@@ -2,12 +2,21 @@ package com.galaxy.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
+
+
+	@GetMapping(value = "/helloWorld")
+	public String getCities() {
+		return "cities";
+	}
 }
